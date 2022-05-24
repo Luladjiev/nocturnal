988c4a35079c2c4dc189f01d4e09f2c433876206
Peter Luladjiev <luladjiev@gmail.com>
2022-05-24 11:30:16 +0300

    Add ExceptionHandlerAdvice

---
+++ src/main/java/com/luladjiev/nocturnal/common/ExceptionHandlerAdvice.java
@@ -0,0 +1,22 @@
+package com.luladjiev.nocturnal.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvice {
+
+  @ExceptionHandler(NotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ResponseBody
+  public ErrorResponse handleNotFoundException(NotFoundException ex) {
+    return new ErrorResponse(
+      ex.getMessage(),
+      HttpStatus.NOT_FOUND,
+      HttpStatus.NOT_FOUND.value()
+    );
+  }
+}
---
+++ src/main/java/com/luladjiev/nocturnal/common/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.luladjiev.nocturnal.common;
+
+public class NotFoundException extends RuntimeException {
+  public NotFoundException(String message) {
+    super(message);
+  }
+}
