969617b5d91e9fbbad6c524c21fe2145f8075377
Peter Luladjiev <luladjiev@gmail.com>
2022-05-24 11:30:52 +0300

    Update projects controller to throw NotFoundException when trying to get invalid Project

--- src/main/java/com/luladjiev/nocturnal/project/ProjectsController.java
+++ src/main/java/com/luladjiev/nocturnal/project/ProjectsController.java
@@ -1,16 +1,16 @@
 package com.luladjiev.nocturnal.project;
 
-import org.springframework.web.bind.annotation.*;
-
+import com.luladjiev.nocturnal.common.NotFoundException;
 import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/projects")
 class ProjectsController {
+
   private final ProjectsService projectsService;
 
   public ProjectsController(ProjectsService projectsService) {
-
     this.projectsService = projectsService;
   }
 
@@ -21,11 +21,20 @@ class ProjectsController {
 
   @GetMapping("/{id}")
   public Project getProject(@PathVariable String id) {
-    return projectsService.getProject(id).orElseThrow();
+    return projectsService
+      .getProject(id)
+      .orElseThrow(() ->
+        new NotFoundException(String.format("Project with ID %s not found", id))
+      );
   }
 
   @PostMapping
   public Project createProject(@RequestBody Project project) {
     return projectsService.createProject(project.getTitle(), project.getId());
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteProject(@PathVariable String id) {
+    projectsService.deleteProject(id);
   }
 }
