31c3e42a4f6ed4720e5742a1e647caf43be46caa
Peter Luladjiev <git@luladjiev.com>
2025-06-04 00:14:49 +0300

    Add Link component

---
+++ Client/src/lib/components/Link/Link.svelte
@@ -0,0 +1,21 @@
+﻿<script lang="ts">
+	export let href;
+</script>
+
+<a {href}>
+	<slot />
+</a>
+
+<style>
+	a {
+		color: #1ea7fd;
+
+		&:hover {
+			text-decoration: underline;
+		}
+
+		&:visited {
+			color: #555ab9;
+		}
+	}
+</style>
